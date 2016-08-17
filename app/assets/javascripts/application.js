d3cee20d3e51bc665f44090524914f76791d1f77
Limitlessprodigy <prodigygit@yahoo.com>
2016-08-17 12:36:00 -0400

    Initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,11 @@
+@import "bootstrap-sprockets";
+@import "bootstrap";
+@import "font-awesome";
+@import "jquery.datetimepicker";
+@import "*";
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
+//= require bindWithDelay
+//= require jquery.datetimepicker
+//= require_tree .
